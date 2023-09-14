391820bdc2ed117beaec8c1cf1ad4e27f2808d55
Jolmos4 <jorgeolmosloya10@hotmail.com>
2023-09-14 13:03:51 -0600

    CASA 6
    make a lot of changes and using codes from previous assignments

--- casa_6.py
+++ casa_6.py
@@ -1,1 +1,21 @@
 filename = './big-mac-full-index.csv'
+
+fhand= open('./big-mac-full-index.csv')
+
+count = 0 
+
+for line in fhand:
+    if "USA" in line:
+        print(line)
+        count += 1
+
+fhand.close()
+print("Total lines with 'USA':", count)
+
+
+
+
+
+
+
+
